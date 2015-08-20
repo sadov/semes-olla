a954fb0ff0367548ee1c40804d55be7355bba4ec
sadov <sadov@localhost.localdomain>
2015-08-20 15:51:04 +0300

    Added CORS rule to header for any host:
    	modified:   wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -121,7 +121,7 @@ def application(environ, start_response):
 
 
     status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body))), ('Access-Control-Allow-Origin', '*')]
     #
     start_response(status, response_headers)
     return [response_body]
